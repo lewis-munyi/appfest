fdb6ee3ef426ba0126d28e747dda3e7ec0efc68b
lewis-munyi <lewismunyi97@gmail.com>
2017-11-03 23:21:09 +0300

    Polishing

--- src/app.js
+++ src/app.js
@@ -1,19 +1,10 @@
-// const config = {
-
-//   apiKey: "AIzaSyA1Xu8zaDZl5HjoI4T4PaUivmOJmA6ZLMo",
-//   authDomain: "smartpeople-9.firebaseapp.com",
-//   databaseURL: "https://smartpeople-9.firebaseio.com",
-//   projectId: "smartpeople-9",
-//   storageBucket: "smartpeople-9.appspot.com",
-//   messagingSenderId: "704037059130"
-// };
 var config = {
-  
+
     apiKey: "AIzaSyA1Xu8zaDZl5HjoI4T4PaUivmOJmA6ZLMo",
     authDomain: "smartpeople-9.firebaseapp.com",
     databaseURL: "https://smartpeople-9.firebaseio.com",
     projectId: "smartpeople-9",
     storageBucket: "smartpeople-9.appspot.com",
     messagingSenderId: "704037059130"
-  };
+};
 firebase.initializeApp(config);
--- src/script.js
+++ src/script.js
@@ -17,8 +17,8 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // Wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
 
-initApp = function () {
-    firebase.auth().onAuthStateChanged(function (user) {
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
             var displayName = user.displayName;
@@ -28,10 +28,10 @@ initApp = function () {
             var uid = user.uid;
             var phoneNumber = user.phoneNumber;
             var providerData = user.providerData;
-            
+
             $("#signincontainer").hide(); //Hide sign in container
             $("#gamecontainer").show(); //Hide sign in container
-            $("#userimage").attr("src",photoURL); //Show user dp
+            $("#userimage").attr("src", photoURL); //Show user dp
             $("#username").text("Hi " + displayName + " here is where you left off"); //Append username
             //More tests
             console.log(emailVerified); //true
@@ -57,153 +57,25 @@ initApp = function () {
             $("#gamecontainer").hide(); //Hide sign in container
             $("#signincontainer").show(); //Hide sign in container
         }
-    }, function (error) {
+    }, function(error) {
         console.log(error);
     });
 };
 
 
 function signout() {
-    firebase.auth().signOut().then(function () {
+    firebase.auth().signOut().then(function() {
         console.log("Sign-out .");
-    }).catch(function (error) {
+    }).catch(function(error) {
         // An error happened.
     });
 }
 
 
-$(document).ready(function () {
+$(document).ready(function() {
     initApp()
-    
+
     /*Materialize*/
-    $(".button-collapse").sideNav(); 
-    $('.tooltipped').tooltip({delay: 50});
+    $(".button-collapse").sideNav();
+    $('.tooltipped').tooltip({ delay: 50 });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function signin() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        name = user.displayName;
-        email = user.email;
-        photoUrl = user.photoURL;
-        emailVerified = user.emailVerified;
-        uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
-
-        console.log(name);
-        console.log(photoUrl);
-        console.log(emailVerified); //true
-        console.log(uid);
-        console.log("Hello!");
-        console.log();
-
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
-
-
-}
-
-function signInWithRedirect() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then(function (result) {
-        if (result.credential) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-        name = user.displayName;
-        email = user.email;
-        photoUrl = user.photoURL;
-        emailVerified = user.emailVerified;
-        uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
-
-        console.log(name);
-        console.log(photoUrl);
-        console.log(emailVerified); //true
-        console.log(uid);
-        console.log("Hello!");
-        console.log();
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
-}
-
-function checkUserStatus() {
-    var user = firebase.auth().currentUser;
-    if (user) {
-        console.log("User is signed in.");
-    } else {
-        // No user is signed in.
-    }
-}
