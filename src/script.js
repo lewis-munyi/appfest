241cd0fd169d608090d49565d7c13549b9342c12
lewis-munyi <lewismunyi97@gmail.com>
2017-11-05 20:54:28 +0300

    App complete.

--- src/script.js
+++ src/script.js
@@ -1,59 +1,107 @@
 function signout() {
-    firebase.auth().signOut().then(function () {
+    firebase.auth().signOut().then(function() {
         console.log("Signed out!");
         $("#gamecontainer").hide(); //Hide sign in container
         $("#signincontainer").show(); //Hide sign in container
         $("#usercontainer").hide(); //Hide user container
-    }).catch(function (error) {
+    }).catch(function(error) {
         console.log("Couldn't sign out");
     });
 }
-
-
 function registeruser(uid, displayName, email) { //Receive the name, id and email values as parameters
     // A student entry.
     var studentData = {
         name: displayName,
         email: email,
-        lastquestion: 0,
+        lastquestion: {
+            general: 0,
+            science: 0,
+            history: 0
+        },
         score: 0
     };
     var updates = {};
     updates['/players/' + uid] = studentData;
     return firebase.database().ref().update(updates); //Register student
 }
-
-
 function checkifuserexists(uid, displayName, email) {
     var userinfo = firebase.database().ref('/players/' + uid);
-    userinfo.on('value', function (snapshot) {
+    userinfo.on('value', function(snapshot) {
         if (JSON.stringify(snapshot) == "null") { //If user doesn't exist
             return registeruser(uid, displayName, email); //Register user
         } else {
             // lastquestion = snapshot.val().lastquestion;
             score = snapshot.val().score;
             var name = snapshot.val().name;
-            var details = name + "<br> Score: " + score + "<br> Highest score: ";
+            var details = name + "<br> Score: " + score + "<br> Leaderboard highscore: ";
             $("#username").text(name); //Append username
-            $("#score").text("Points: " + score); //Append user points
+            $("#score").text("Your total points: " + score); //Append user points
         }
     });
     var userId = firebase.auth().currentUser.uid;
 }
-
-function playgame(category) {
+function verifyanswer() {
+    var answer = document.querySelector('input[name = "options"]:checked').value;
+    if (answer == legitanswer) {
+        Materialize.toast("Answer " + answer + " is correct!", 1000);
+        console.log("Question points:" + questionpoints);
+        console.log("score: " + score);
+        newScore = score + questionpoints;
+        console.log("new score: " + newScore);
+        if (questioncategory == "science") {
+            var updates = {};
+            updates['players/' + uid + "/lastquestion/" + questioncategory] = currentquestion;
+            firebase.database().ref().update(updates);
+        } else if (questioncategory == "general") {
+            var updates = {};
+            updates['players/' + uid + "/lastquestion/" + questioncategory] = currentquestion;
+            firebase.database().ref().update(updates);
+        } else if (questioncategory == "history") {
+            var updates = {};
+            updates['players/' + uid + "/lastquestion/" + questioncategory] = currentquestion;
+            firebase.database().ref().update(updates);
+        }
+        var updates = {};
+        updates['players/' + uid + '/score'] = newScore;
+        firebase.database().ref().update(updates);
+        playgame();
+    } else {
+        Materialize.toast("Incorrect! The correct answer is option " + answer, 1000);
+        if (questioncategory == "science") {
+            var updates = {};
+            updates['players/' + uid + "/lastquestion/" + questioncategory] = currentquestion;
+            firebase.database().ref().update(updates);
+        } else if (questioncategory == "general") {
+            var updates = {};
+            updates['players/' + uid + "/lastquestion/" + questioncategory] = currentquestion;
+            firebase.database().ref().update(updates);
+        } else if (questioncategory == "history") {
+            var updates = {};
+            updates['players/' + uid + "/lastquestion/" + questioncategory] = currentquestion;
+            firebase.database().ref().update(updates);
+        }
+        playgame();
+    }
+}
+function playgame() {
     $("#category").hide();
     $("#gamecontainer").show();
-    questioncategory = category;
-    var userinfo = firebase.database().ref('/players/' + uid);
-    userinfo.on('value', function (snapshot) {
-        lastquestion = snapshot.val().lastquestion;
-        score = snapshot.val().score;
+    console.log(questioncategory)
+    var getlastquestion = firebase.database().ref('/players/' + uid + "/lastquestion/");
+    getlastquestion.on('value', function(snapshot) {
+        lastquestionsobject = snapshot.val();
+        console.log(JSON.stringify(lastquestionsobject));
+        if (questioncategory == "science") {
+            currentquestion = lastquestionsobject.science + 1;
+            console.log(currentquestion);
+        } else if (questioncategory == "general") {
+            currentquestion = lastquestionsobject.general + 1;
+        } else if (questioncategory == "history") {
+            currentquestion = lastquestionsobject.history + 1;
+        }
     });
-    currentquestion = lastquestion + 1;
-    
     $("#questionNumber").text(currentquestion);
-    firebase.database().ref('/questions/' + questioncategory + "/" + currentquestion).once('value').then(function (snapshot) {
+    firebase.database().ref('/questions/' + questioncategory + "/" + currentquestion).once('value').then(function(snapshot) {
         var questions = snapshot.val();
         legitanswer = questions.answer;
         questionpoints = questions.points;
@@ -65,46 +113,22 @@ function playgame(category) {
         $("#loption3").text(questions.options[3]);
         $("#loption4").text(questions.options[4]);
         console.log(questions.question);
-    });
-    // #26A69A teal
-    // pink #E91E63
+    });// #26A69A teal pink #E91E63
 }
-
-function verifyanswer() {
-    var answer = document.querySelector('input[name = "options"]:checked').value;
-    if (answer == legitanswer) {
-        Materialize.toast("Answer " + answer + " is correct!", 1000);
-        console.log("Question points:" + questionpoints);
-        console.log("score: " + score);
-        newScore = score + questionpoints;
-        console.log("new score: " + newScore);
-        firebase.database().ref('players/' + uid).set({
-            name: displayName,
-            email: email,
-            lastquestion: currentquestion,
-            score: newScore
-        });
-        playgame(questioncategory);
-    } else {
-        Materialize.toast("Answer " + answer + " is wrong!", 1000);
-    }
 }
-
 function loadcategory(category) {
     if (category == 1) {
-        category = "general";
+        questioncategory = "general";
     } else if (category == 2) {
-        category = "science";
+        questioncategory = "science";
     } else {
-        category = "history";
+        questioncategory = "history";
     }
-    Materialize.toast("Loading " + category + " quizzes", 100);
-    playgame(category);
+    Materialize.toast("Loading " + questioncategory + " quizzes", 1000);
+    playgame();
 };
-
 const auth = firebase.auth(); // firebase.initializeApp(config);
 var database = firebase.database();
-
 // FirebaseUI config.
 var uiConfig = {
     signInSuccessUrl: '/',
@@ -119,12 +143,11 @@ $("#category").hide();
 $("#gamecontainer").hide(); //Hide sign in container
 
 //Declare global variables
-var displayName, emailVerified, photoURL, uid, phoneNumber, providerData, lastquestion, legitanswer, questionpoints, score, currentquestion, questioncategory;
+var displayName, emailVerified, photoURL, uid, phoneNumber, providerData, lastquestion, legitanswer, questionpoints, highscore, score, currentquestion, lastquestionsobject, questioncategory;
 var ui = new firebaseui.auth.AuthUI(firebase.auth()); // Initialize the FirebaseUI Widget using Firebase.
 ui.start('#firebaseui-auth-container', uiConfig); // Wait until the DOM is loaded.
-
-initApp = function () {
-    firebase.auth().onAuthStateChanged(function (user) {
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
             displayName = user.displayName;
@@ -139,26 +162,35 @@ initApp = function () {
             $("#gamecontainer").hide(); //Hide sign in container
             $("#userimage").attr("src", photoURL); //Show user dp
             // $("#username").text("Hi " + displayName + " here is where you left off"); //Append username
-
             checkifuserexists(uid, displayName, email); //First check if the crrent user has registered
-            // playgame();
-            //More code
         } else {
             // User is signed out.
             $("#gamecontainer").hide(); //Hide sign in container
             $("#usercontainer").hide(); //Hide user container
             $("#signincontainer").show(); //Show sign in container
         }
-    }, function (error) {
+    }, function(error) {
         console.log(error);
     });
 };
-$(document).ready(function () {
-    initApp()
+var listentoscore = firebase.database().ref('players/');
+listentoscore.on('child_changed', function(data) {
+    if (score > highscore) {
+        firebase.database().ref('highscore/').set({
+            name: displayName,
+            email: email,
+            score: score
+        });
+    }
+});
+var gethighscores = firebase.database().ref('/highscore');
+gethighscores.on('value', function(snapshot) {
+    highscore = snapshot.val().score;
+    $('#highscore').text("Highest score: " + highscore);
+});
 
-    /*Materialize*/
-    $(".button-collapse").sideNav();
-    $('.tooltipped').tooltip({
-        delay: 50
-    });
+$(document).ready(function() {
+    initApp()
+    $(".button-collapse").sideNav(); /*Materialize*/
+    $('.tooltipped').tooltip({delay: 50});
 })
