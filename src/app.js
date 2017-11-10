b9ee3a5714c9ffd22f55789370631d8840bfdb9b
lewis-munyi <lewismunyi97@gmail.com>
2017-11-10 09:55:23 +0300

    My config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 var config = {
-    apiKey: "AIzaSyA1Xu8zaDZl5HjoI4T4PaUivmOJmA6ZLMo",
-    authDomain: "smartpeople-9.firebaseapp.com",
-    databaseURL: "https://smartpeople-9.firebaseio.com",
-    projectId: "smartpeople-9",
-    storageBucket: "smartpeople-9.appspot.com",
-    messagingSenderId: "704037059130"
-};
+    apiKey: "AIzaSyAfhO_8iroaOMHSansqAziU3K1IpolQts8",
+    authDomain: "appfest-e3104.firebaseapp.com",
+    databaseURL: "https://appfest-e3104.firebaseio.com",
+    projectId: "appfest-e3104",
+    storageBucket: "appfest-e3104.appspot.com",
+    messagingSenderId: "57986137591"
+  };
 firebase.initializeApp(config);
